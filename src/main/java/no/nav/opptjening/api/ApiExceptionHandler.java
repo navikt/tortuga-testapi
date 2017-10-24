a9b20a4e3f1241e65f4f8bfbbade1336a3769618
David Steinsland <github@davidsteinsland.net>
2017-10-24 15:07:36 +0200

    Reduce log level and noise

--- src/main/java/no/nav/opptjening/api/ApiExceptionHandler.java
+++ src/main/java/no/nav/opptjening/api/ApiExceptionHandler.java
@@ -1,6 +1,5 @@
 package no.nav.opptjening.api;
 
-import no.nav.opptjening.api.hendelser.HendelseService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -10,11 +9,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ApiExceptionHandler {
 
-    private static final Logger LOG = LoggerFactory.getLogger(HendelseService.class);
+    private static final Logger LOG = LoggerFactory.getLogger(ApiExceptionHandler.class);
 
     @ExceptionHandler(ApiException.class)
     public ResponseEntity<ErrorResponse> exceptionHandler(ApiException ex) {
-        LOG.warn(ex.getMessage(), ex);
+        LOG.info("HttpStatus: {}, melding: {}", ex.getHttpStatus(), ex.getMessage());
         return new ResponseEntity<ErrorResponse>(new ErrorResponse(ex), ex.getHttpStatus());
     }
 }
